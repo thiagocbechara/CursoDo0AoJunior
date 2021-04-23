3c53d6a933d8793c0f9a1becbcdc30116c9c130b
Thiago Capibaribe Bechara <thiagobechara90@hotmail.com>
2021-04-22 21:11:56 -0300

    Adicionado testes unitários do projeto games-api

---
+++ Testes/Games.Infra.Test/Repository/GameResultRepositoryTest.cs
@@ -0,0 +1,58 @@
+﻿using Games.Domain.Models;
+using Games.Infra.Db;
+using Games.Infra.Repository.Implementations;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Games.Infra.Test.Repository
+{
+    public class GameResultRepositoryTest
+    {
+        private ApplicationDbContext MockContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("FakeConnectionString")
+                .Options;
+            MockContext = new ApplicationDbContext(dbContextOptions);
+        }
+
+        private void ClearDb()
+        {
+            MockContext.GameResults.RemoveRange(MockContext.GameResults);
+            MockContext.SaveChanges();
+        }
+
+        [Test]
+        public void SaveOrUpdate_When_New_Result_Success()
+        {
+            ClearDb();
+            var repository = new GameResultRepository(MockContext);
+
+            var fakeGameResult = new GameResult { GameId = 1, PlayerId = 1, Win = 1 };
+            Assert.DoesNotThrowAsync(() => repository.SaveOrUpdate(fakeGameResult));
+            var fakeDbResult = MockContext.GameResults.FirstOrDefault(x => x.PlayerId == 1 && x.GameId == 1);
+            Assert.NotNull(fakeDbResult);
+        }
+
+        [Test]
+        public void SaveOrUpdate_When_Result_Success()
+        {
+            ClearDb();
+            MockContext.GameResults.Add(new GameResult { GameId = 1, PlayerId = 1, Win = 1 });
+            MockContext.SaveChanges();
+
+            var fakeGameResult = new GameResult { GameId = 1, PlayerId = 1, Win = 1 };
+
+            var repository = new GameResultRepository(MockContext);
+            Assert.DoesNotThrowAsync(() => repository.SaveOrUpdate(fakeGameResult));
+
+            var fakeDbResult = MockContext.GameResults.FirstOrDefault(x => x.PlayerId == 1 && x.GameId == 1);
+            Assert.NotNull(fakeDbResult);
+            Assert.AreEqual(2, fakeDbResult.Win);
+        }
+    }
+}
---
+++ Testes/Games.Infra.Test/Repository/LeaderboardRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using Games.Domain.Models;
+using Games.Infra.Db;
+using Games.Infra.Repository.Implementations;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Games.Infra.Test.Repository
+{
+    public class LeaderboardRepositoryTest
+    {
+        private ApplicationDbContext MockContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("FakeConnectionString")
+                .Options;
+            MockContext = new ApplicationDbContext(dbContextOptions);
+        }
+
+        private void ClearDb()
+        {
+            MockContext.GameResults.RemoveRange(MockContext.GameResults);
+            MockContext.SaveChanges();
+        }
+
+        [Test]
+        public void Top100_Returns_10_Results()
+        {
+            ClearDb();
+            for(var i = 0; i < 10; i++)
+            {
+                MockContext.GameResults.Add(new GameResult { PlayerId = i + 1, GameId = 1, Win = i });
+            }
+            MockContext.SaveChanges();
+
+            var repository = new LeaderboardRepository(MockContext);
+            var leader = repository.GetTop100(1);
+
+            Assert.AreEqual(10, leader.Count);
+            var first = leader.FirstOrDefault();
+            var last = leader.LastOrDefault();
+
+            Assert.AreEqual(9, first.Balance);
+            Assert.AreEqual(0, last.Balance);
+        }
+    }
+}
